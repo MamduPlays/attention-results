faddda156e82aa8f9e90a8e7c5b5e2b34158847d
Muhammad Ahmad <114693183+MamduPlays@users.noreply.github.com>
2026-01-22 06:39:35 +0000

    msg

--- api/save.js
+++ api/save.js
@@ -1,13 +1,12 @@
 import { createClient } from '@supabase/supabase-js'
-
 const supabase = createClient(process.env.SB_URL, process.env.SB_KEY)
 
 export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     if (req.method === 'POST') {
-        const { status } = req.body;
-        await supabase.from('study_logs').insert([{ status }]);
+        const { time, status } = req.body; // Catches the 'time' now
+        await supabase.from('study_logs').insert([{ time_label: time, status: status }]);
         return res.status(200).json({ success: true });
     }
     res.status(405).send("Method Not Allowed");
